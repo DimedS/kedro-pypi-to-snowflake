542a82bbda737c6cf40ef0e5632a7ee0d125d670
Dmitry Sorokin <dmd40in@gmail.com>
2024-10-01 16:47:23 +0100

    Change cron and data type
    Signed-off-by: Dmitry Sorokin <dmd40in@gmail.com>

--- src/kedro_pypi_to_snowflake/pipelines/etl/nodes.py
+++ src/kedro_pypi_to_snowflake/pipelines/etl/nodes.py
@@ -2,8 +2,10 @@
 This is a boilerplate pipeline 'etl'
 generated using Kedro 0.19.8
 """
+import pandas as pd
 import pyarrow as pa
 
-
 def convert_arrow_to_snowpark(data: pa.Table):
-    return data.to_pandas()
+    df = data.to_pandas()
+    df['date'] = pd.to_datetime(df['date'], unit='D', origin='1970-01-01')
+    return df
